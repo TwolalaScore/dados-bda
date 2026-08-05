4ed5fee8bd03f9a5808291d234b4e666db1b3778
TwolalaScore <herickcostasouza1996@gmail.com>
2026-08-05 18:50:37 -0300

    Add files via upload

---
+++ build-db.js
@@ -0,0 +1,19 @@
+EDIÇÕES SEM FINAL/CAMPEÃO REGISTRADO (dados incompletos na planilha original):
+  - 26ª (id ed25)
+  - 272ª ok (id ed271)
+  - 346ª ok (id ed345)
+
+EDIÇÕES SEM REGISTRO DE CONFRONTOS (só o campeão é conhecido, sem chaveamento):
+  - 1ª: campeão = MENOR SP
+  - 2ª  : campeão = RUIVO
+  - 3ª: campeão = THIAGO
+  - 4ª: campeão = BAHIA
+  - 5ª: campeão = KANT
+  - 9ª: campeão = TATU
+  - 11ª: campeão = THIAGO
+  - 14ª: campeão = REFEL
+  - 35ª: campeão = LC
+
+EDIÇÕES MARCADAS COMO CANCELADAS na planilha original:
+  - 26ª
+  - 272ª ok
